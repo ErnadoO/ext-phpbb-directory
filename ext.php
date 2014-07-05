67b11a849b383b2d826f53ea02e540182ccae713
Erwan Nader <ernadoo@gmail.com>
2014-07-05 23:38:14 +0200

    Update ext class to handle notifications during en/disable/purge

--- ext.php
+++ ext.php
@@ -15,4 +15,99 @@ namespace ernadoo\phpbbdirectory;
 
 class ext extends \phpbb\extension\base
 {
+	/**
+	 * Single enable step that installs any included migrations
+	 *
+	 * @param mixed $old_state State returned by previous call of this method
+	 * @return mixed Returns false after last step, otherwise temporary state
+	 */
+	function enable_step($old_state)
+	{
+		switch ($old_state)
+		{
+			case '': // Empty means nothing has run yet
+
+				// Enable board rules notifications
+				$phpbb_notifications = $this->container->get('notification_manager');
+				$phpbb_notifications->enable_notifications('directory_website');
+				$phpbb_notifications->enable_notifications('directory_website_activate');
+				$phpbb_notifications->enable_notifications('directory_website_delete');
+				$phpbb_notifications->enable_notifications('directory_website_error_cron');
+				$phpbb_notifications->enable_notifications('directory_website_in_queue');
+				return 'notifications';
+
+				break;
+
+			default:
+
+				// Run parent enable step method
+				return parent::enable_step($old_state);
+
+				break;
+		}
+	}
+
+	/**
+	 * Single disable step that does nothing
+	 *
+	 * @param mixed $old_state State returned by previous call of this method
+	 * @return mixed Returns false after last step, otherwise temporary state
+	 */
+	function disable_step($old_state)
+	{
+		switch ($old_state)
+		{
+			case '': // Empty means nothing has run yet
+
+				// Disable board rules notifications
+				$phpbb_notifications = $this->container->get('notification_manager');
+				$phpbb_notifications->disable_notifications('directory_website');
+				$phpbb_notifications->disable_notifications('directory_website_activate');
+				$phpbb_notifications->disable_notifications('directory_website_delete');
+				$phpbb_notifications->disable_notifications('directory_website_error_cron');
+				$phpbb_notifications->disable_notifications('directory_website_in_queue');
+				return 'notifications';
+
+				break;
+
+			default:
+
+				// Run parent disable step method
+				return parent::disable_step($old_state);
+
+				break;
+		}
+	}
+
+	/**
+	 * Single purge step that reverts any included and installed migrations
+	 *
+	 * @param mixed $old_state State returned by previous call of this method
+	 * @return mixed Returns false after last step, otherwise temporary state
+	 */
+	function purge_step($old_state)
+	{
+		switch ($old_state)
+		{
+			case '': // Empty means nothing has run yet
+
+				// Purge board rules notifications
+				$phpbb_notifications = $this->container->get('notification_manager');
+				$phpbb_notifications->purge_notifications('directory_website');
+				$phpbb_notifications->purge_notifications('directory_website_activate');
+				$phpbb_notifications->purge_notifications('directory_website_delete');
+				$phpbb_notifications->purge_notifications('directory_website_error_cron');
+				$phpbb_notifications->purge_notifications('directory_website_in_queue');
+				return 'notifications';
+
+				break;
+
+			default:
+
+				// Run parent purge step method
+				return parent::purge_step($old_state);
+
+				break;
+		}
+	}
 }
