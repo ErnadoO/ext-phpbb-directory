52e4be002b7dcdbf3ecfa0224ceb7dad7736a6a6
Erwan Nader <ernadoo@gmail.com>
2014-11-09 19:19:06 +0100

    update "DEVELOPERS PLEASE NOTE" section

--- language/en/info_acp_directory.php
+++ language/en/info_acp_directory.php
@@ -29,9 +29,9 @@ if (empty($lang) || !is_array($lang))
 // 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
 // translators to re-order the output of data while ensuring it remains correct
 //
-// You do not need this when single placeholders are used, e.g. 'Message %d' is fine
-// equally when a string contains only two placeholders which are used to wrap text
-// in a url once again you do not need to specify an order e.g., 'Click %sHERE%s' is fine
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 
 $lang = array_merge($lang, array(
 	'ACP_DIRECTORY'							=> 'phpBB Directory',
