b5f560fca1fdca80344f812690450a1a763515a5
Erwan Nader <ernadoo@gmail.com>
2016-03-27 17:03:41 +0200

    Remove fix for PHPBB3-14381

--- textreparser/plugins/cat_description.php
+++ textreparser/plugins/cat_description.php
@@ -24,31 +24,4 @@ class cat_description extends \phpbb\textreparser\row_based_plugin
 			'options'    => 'cat_desc_options',
 		);
 	}
-
-	/**
-	* {@inheritdoc}
-	*/
-	protected function get_records_by_range_query($min_id, $max_id)
-	{
-		$columns = $this->get_columns();
-		$fields  = array();
-		foreach ($columns as $field_name => $column_name)
-		{
-			if ($column_name === $field_name)
-			{
-				$fields[] = $column_name;
-			}
-			else
-			{
-				$fields[] = $column_name . ' AS ' . $field_name;
-			}
-		}
-
-		$sql = 'SELECT ' . implode(', ', $fields) . '
-			FROM ' . $this->table . '
-			WHERE ' . $columns['id'] . ' BETWEEN ' . $min_id . ' AND ' . $max_id . '
-				AND ' . $columns['text'] . ' <> ""';
-
-		return $sql;
-	}
 }
