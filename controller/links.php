cc72bbe9e7568669f883f553b26496471cfc0b98
ErnadoO <ernadoo@gmail.com>
2016-08-29 14:10:30 +0200

    Fix bbcode issue in desciption

--- controller/links.php
+++ controller/links.php
@@ -473,7 +473,7 @@ class links
 
 			if ($this->description)
 			{
-				generate_text_for_storage($this->description, $data_edit['link_uid'], $data_edit['link_bitfield'], $data_edit['link_flags'], (bool) $this->config['allow_bbcode'], (bool) $this->config['allow_post_links'], (bool) $this->config['allow_smilies']);
+				generate_text_for_storage($data_edit['link_description'], $data_edit['link_uid'], $data_edit['link_bitfield'], $data_edit['link_flags'], (bool) $this->config['allow_bbcode'], (bool) $this->config['allow_post_links'], (bool) $this->config['allow_smilies']);
 			}
 
 			$need_approval = ($this->categorie->need_approval() && !$this->auth->acl_get('a_') && !$this->auth->acl_get('m_')) ? true : false;
